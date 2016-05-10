84cc88f52abee34c8f12709bbd5f75e79df0f28a
guAnsunyata <guansunyata@gmail.com>
2016-05-10 10:33:37 +0800

    add koa server

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),               // 載入 gulp
     gulpSass = require('gulp-sass');    // 載入 gulp-sass
 
 gulp.task('watch', function () {
-    gulp.watch('scss/*.scss', ['styles']);
+    gulp.watch('./public/scss/*.scss', ['styles']);
 });
 
 gulp.task('styles', function () {
-    gulp.src('scss/*.scss')    // 指定要處理的 Scss 檔案目錄
+    gulp.src('./public/scss/*.scss')    // 指定要處理的 Scss 檔案目錄
         .pipe(gulpSass({          // 編譯 Scss
             outputStyle: 'compressed'
         }))         
-        .pipe(gulp.dest('css'));  // 指定編譯後的 css 檔案目錄
+        .pipe(gulp.dest('./public/css'));  // 指定編譯後的 css 檔案目錄
 });
